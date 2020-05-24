e880e37e58608a8f3cd23ffa93c7bf7af58bb244
Diogo Ferreira <diogo.mart.ferreira@gmail.com>
2020-05-24 17:38:05 +0100

    [feat] Add theme-ui, styles.css and create next _app to add providers

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,25 @@
+/** @jsx jsx */
+import { jsx, Container } from "theme-ui";
 import Head from "next/head";
 
 export default function Home() {
   return (
-    <div>
+    <Container>
       <Head>
         <title>Diogo - Thoughts on life, design and code.</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1>Thoughts on life, design and code.</h1>
-
-      <style jsx global>{`
-        @import url("https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap");
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: "Inter", -apple-system, BlinkMacSystemFont, Segoe UI,
-            Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans,
-            Helvetica Neue, sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </div>
+      <h1
+        sx={{
+          fontWeight: "regular",
+          fontSize: [5, 6, 8], // picks up value from `theme.fontSizes[4]`
+          color: "primary", // picks up value from `theme.colors.primary`
+          p: 4,
+        }}
+      >
+        Thoughts on life, design and code.
+      </h1>
+    </Container>
   );
 }
