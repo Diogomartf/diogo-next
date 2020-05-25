3e55484b21107e0d462b7bb43899a4d43826f547
Diogo Ferreira <diogo.mart.ferreira@gmail.com>
2020-05-25 23:19:20 +0100

    [feat] Add darkmode with theme-ui

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,30 @@
 /** @jsx jsx */
-import { jsx, Container } from "theme-ui";
+import { jsx, Container, Box, Flex } from "theme-ui";
 import Head from "next/head";
+import ToogleDarkMode from "../toogleDarkMode";
 
 export default function Home() {
   return (
-    <Container>
+    <div>
       <Head>
         <title>Diogo - Thoughts on life, design and code.</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <h1
-        sx={{
-          fontWeight: "regular",
-          fontSize: [5, 6, 8], // picks up value from `theme.fontSizes[4]`
-          color: "primary", // picks up value from `theme.colors.primary`
-          p: 4,
-        }}
-      >
-        Thoughts on life, design and code.
-      </h1>
-    </Container>
+      <Box p={5}>
+        <Flex justifyContent="flex-end">
+          <ToogleDarkMode />
+        </Flex>
+        <h1
+          sx={{
+            fontWeight: "regular",
+            fontSize: [5, 6, 8], // picks up value from `theme.fontSizes[4]`
+            color: "primary", // picks up value from `theme.colors.primary`
+            py: 1,
+          }}
+        >
+          Thoughts on life, design and code.
+        </h1>
+      </Box>
+    </div>
   );
 }
