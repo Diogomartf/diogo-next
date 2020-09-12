6724e35909ad1572192611cd335d38e0c8d86352
Diogo Ferreira <diogo.mart.ferreira@gmail.com>
2020-09-12 11:03:23 +0100

    [Feature] Add favicon, update next and update hero

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,46 @@
 import { ThemeProvider } from "theme-ui";
+import Head from "next/head";
 import theme from "../theme";
 import "../styles.css";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ThemeProvider theme={theme}>
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <>
+      <Head>
+        <title>Diogo - Thoughts on life, design and code.</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="author" content="Diogo Ferreira" />
+        <meta
+          name="description"
+          content="I’m Diogo, a Software Engineer currently living in Braga, Portugal."
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta name="msapplication-config" content="/browserconfig.xml" />
+        <meta name="msapplication-TileColor" content="#FFA592" />
+        <meta name="theme-color" content="#FFA592"></meta>
+      </Head>
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
   );
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,3 @@
-import Head from "next/head";
 import Hero from "../components/Hero";
 import Container from "../components/Container";
 import Header from "../components/Header";
@@ -6,10 +5,6 @@ import Header from "../components/Header";
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>Diogo - Thoughts on life, design and code.</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <Container>
         <Header></Header>
         <Hero />
