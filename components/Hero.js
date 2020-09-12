4964a8e5fdde09c40415c06ec8b702f368021e45
Diogo Ferreira <diogo.mart.ferreira@gmail.com>
2020-09-12 11:02:26 +0100

    [Feature] Add favicon, update next and update hero

--- components/Hero.js
+++ components/Hero.js
@@ -6,16 +6,22 @@ const Hero = () => (
     <h1
       sx={{
         fontWeight: "regular",
-        fontSize: [5, 7],
+        fontSize: [4, 5],
         color: "primary",
         my: 3,
       }}
     >
-      Thoughts on life, code and design.
+      Hello. I'm Diogo.
     </h1>
     <Text sx={{ lineHeight: "160%", fontSize: [2, 3] }}>
-      I'm Diogo Ferreira, a Full-stack web developer and designer. Currently
-      working as Frontend web developer and designer at Q-Better.
+      I'm a Portuguese Full-stack web developer.
+    </Text>
+    <Text sx={{ lineHeight: "160%", fontSize: [2, 3] }}>
+      Currently writting react apps at Q-Better.
+    </Text>
+    <Text sx={{ lineHeight: "160%", fontSize: [2, 3] }} my={[2, 3]}>
+      I try to follow my curiousity which lately has been around{" "}
+      <code>computers</code>, <code>systems</code>, <code>how to live</code>.
     </Text>
   </Box>
 );
